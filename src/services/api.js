f79218c31cf33fc28a850589672b76d9acf03dc3
test <test@yopmail.com>
2025-07-10 16:14:27 +0530

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://crypto-backend-new-osvy.onrender.com/api';
+// const API_URL = 'https://crypto-backend-new-osvy.onrender.com/api';
+const API_URL = 'http://localhost:5000/api';
 
 export const fetchCoins = async () => {
     try {
